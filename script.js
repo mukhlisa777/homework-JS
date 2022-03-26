e214f88716ecf2db82105845df2703f207a8e904
mukhlisa <mukhlis.777.88@gmail.com>
2022-03-26 12:13:42 +0500

    homework

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let answer = 'true answer is '
+let num = '2'
+let numm = '5'
+let your = 'your answer is '
+let prob = prompt('2 * 5 = ?')
+console.log(your+prob);
+console.log(answer+2*5);
+
+
+
+let ans = 'true answer is '
+let n = '10'
+let nr = '5'
+let you = 'your answer is '
+let pb = prompt('10 / 5 = ?')
+console.log(you+pb);
+console.log(ans+n/nr);
+  
+
+let sav = 'true answer is '
+let nb = '20'
+let rm = '50'
+let jav = 'your answer is '
+let mas = prompt('20 * 50 = ?')
+console.log(jav+mas);
+console.log(sav + nb * rm);
+ 
+
+
+let ном = 'true answer is '
+let нн = 10
+let про = 5
+let вы = 'your answer is '
+let воп = prompt('10 + 5 = ?')
+console.log(вы+воп);
+console.log( ном + (нн + про));
+
+
+
